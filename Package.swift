b240867de710785703a7b2d79fac44f1e4848dd7
runner <runner@Mac-1698596402703.local>
2023-10-29 17:16:43 +0000

    Update Pacakge.swift 20231029171428

--- Package.swift
+++ Package.swift
@@ -19,23 +19,23 @@ let package = Package(
       
       .binaryTarget(
         name: "App",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029165733/App.xcframework.zip",
-        checksum: "ffa0341ebee959a06d40d17ce6273c72d28d2199f66938f2139301ff27994782"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029171428/App.xcframework.zip",
+        checksum: "1daa0350d2d4c4a719241b326178ee4c913923604bb3d77617822901202e5540"
       ),
       .binaryTarget(
         name: "Flutter",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029165733/Flutter.xcframework.zip",
-        checksum: "92d52ee6c23cc56255385796cfaddf9ffa557341694d938c30d637450bb3a2b1"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029171428/Flutter.xcframework.zip",
+        checksum: "55aed7f01f7d30f78f4c1c7a6f4f4ee409aae3e4f66a56e1a5085043d3ec7635"
       ),
       .binaryTarget(
         name: "FlutterPluginRegistrant",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029165733/FlutterPluginRegistrant.xcframework.zip",
-        checksum: "cf87d9bc1632e5c95ac5f8e531b9b80abae34bcbf30dce130cd849fad6fcbc73"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029171428/FlutterPluginRegistrant.xcframework.zip",
+        checksum: "77b5e20385a665613d755219469bdef8f8896aa7ffa0cf1bc1e5ccb61e123bf3"
       ),
       .binaryTarget(
         name: "native_plugin",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029165733/native_plugin.xcframework.zip",
-        checksum: "47dd675637107d25525361ec6cb5798a6a989ca27b8d3f345183e2e6560e16fa"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/20231029171428/native_plugin.xcframework.zip",
+        checksum: "4af64c67988acb717ed7ac708f8f5eba3c763b278be1a4e7453b71e717351a5f"
       )
     ],
     swiftLanguageVersions: [.v5]
