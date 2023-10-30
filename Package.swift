66f8db7c9ff12ed8c82086d9048b7776e58142f9
runner <runner@Mac-1698676752330.local>
2023-10-30 14:52:45 +0000

    Update Pacakge.swift 1.0.3

--- Package.swift
+++ Package.swift
@@ -19,23 +19,23 @@ let package = Package(
       
       .binaryTarget(
         name: "App",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.2/App.xcframework.zip",
-        checksum: "7766b1054b36d40859435f0ed2c601ddd4aeb324f8537b896dbb2b74434497da"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.3/App.xcframework.zip",
+        checksum: "574c4464bdc50914217fbb27f7926fd00b567aa687cd205f3b68a1e2835ae38f"
       ),
       .binaryTarget(
         name: "Flutter",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.2/Flutter.xcframework.zip",
-        checksum: "7452a63c796fc0779629ea13c0967de04adc03d4e71da8bdc8c5cbd16b77e63f"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.3/Flutter.xcframework.zip",
+        checksum: "c42acc546b2b63fe421b887f828922edb13c442efabb9fdb73689212c16f66f9"
       ),
       .binaryTarget(
         name: "FlutterPluginRegistrant",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.2/FlutterPluginRegistrant.xcframework.zip",
-        checksum: "60c376d31525620ece3e0d1d3369db03b5f72e7f289192b194b3ade21f0927e2"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.3/FlutterPluginRegistrant.xcframework.zip",
+        checksum: "2529545e28ec934327126dabf2147f63f6fce0fe9d109fb06d2fcda283504c30"
       ),
       .binaryTarget(
         name: "native_plugin",
-        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.2/native_plugin.xcframework.zip",
-        checksum: "6ed96f98fff07209b1845275915d8f17d6ed65f66d059958e127d67d700ba8e9"
+        url: "https://github.com/Atsumi3/Flutter-Deploy-XCFramework/releases/download/1.0.3/native_plugin.xcframework.zip",
+        checksum: "b82c9ca5ee23031f450c925eb632d4cc6092362577ecbd8050c8a2e67bf42994"
       )
     ],
     swiftLanguageVersions: [.v5]
